03abb44aa1615e9ea0a14021f80ee6605bda4e89
swarnanil bera <iamsb.1104@gmail.com>
2024-10-20 19:14:29 +0530

    case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SideBar from './components/sidebar/sideBar'
+import SideBar from './components/Sidebar'
 import Main from './components/Main/main'
 
 const App = () => {
